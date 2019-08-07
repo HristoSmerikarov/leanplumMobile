71cb1e35a2f242000db35632b263d8c51c6e08f8
MUSALA\hristo.smerikarov <hristo.smerikarov@musala.com>
2019-08-07 11:07:09 +0300

    Updates for ios

--- src/test/java/com/leanplum/AppTest.java
+++ src/test/java/com/leanplum/AppTest.java
@@ -14,27 +14,27 @@ import io.appium.java_client.ios.IOSDriver;
 
 public class AppTest extends BaseTest {
 
-    @Test
-    public void android() {
-        AndroidDriver<MobileElement> driver = (AndroidDriver<MobileElement>) getAppiumDriver();
-        
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/button1")));
-
-        driver.findElementById("android:id/button1").click();
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.leanplum.rondo:id/adhoc")));
-        driver.findElementById("com.leanplum.rondo:id/adhoc").click();
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.leanplum.rondo:id/buttonTrack")));
-        String buttonText = getAppiumDriver().findElementById("com.leanplum.rondo:id/buttonTrack").getText();
-
-        Assert.assertTrue(buttonText.equals("SEND TRACK EVENT"));
-        
-        driver.closeApp();
-        
-        driver.quit();
-    }
+//    @Test
+//    public void android() {
+//        AndroidDriver<MobileElement> driver = (AndroidDriver<MobileElement>) getAppiumDriver();
+//        
+//        WebDriverWait wait = new WebDriverWait(driver, 20);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/button1")));
+//
+//        driver.findElementById("android:id/button1").click();
+//
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.leanplum.rondo:id/adhoc")));
+//        driver.findElementById("com.leanplum.rondo:id/adhoc").click();
+//
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.leanplum.rondo:id/buttonTrack")));
+//        String buttonText = getAppiumDriver().findElementById("com.leanplum.rondo:id/buttonTrack").getText();
+//
+//        Assert.assertTrue(buttonText.equals("SEND TRACK EVENT"));
+//        
+//        driver.closeApp();
+//        
+//        driver.quit();
+//    }
     
     @Test
     public void ios() {
