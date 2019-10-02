993317fab84017a6c71290898faa42b57fd693f6
hristogergov <hristogergov@10.6.0.70>
2019-10-02 09:41:16 +0300

    Updates for VariablesPO

--- src/main/java/com/leanplum/tests/pageobject/VariablesPO.java
+++ src/main/java/com/leanplum/tests/pageobject/VariablesPO.java
@@ -16,71 +16,78 @@ import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 
 public class VariablesPO extends BasePO {
 
-    private static final String ANDROID_IMAGE_XPATH = "";
-    private static final String IOS_IMAGE_XPATH = "";
+	private static final String IOS_VAR_STRING_XPATH = "//XCUIElementTypeStaticText[@name='varString:']";
+	private static final String IOS_VAR_NUMBER_XPATH = "//XCUIElementTypeStaticText[@name='varNumber:']";
+	private static final String IOS_VAR_BOOL_XPATH = "//XCUIElementTypeStaticText[@name='varBool']";
+	private static final String IOS_VAR_FILE_XPATH = "//XCUIElementTypeStaticText[@name='varFile:']";
+	private static final String IOS_VAR_FILE_IMAGE_XPATH = "//XCUIElementTypeStaticText[@name='varFile (image):']";
+	private static final String FOLLOWING_SIBLING_TEXT_XPATH = "/following-sibling::XCUIElementTypeStaticText";
+	private static final String FOLLOWING_SIBLING_IMAGE_XPATH = "/following-sibling::XCUIElementTypeImage";
+	private static final String ANDROID_IMAGE_XPATH = "";
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varStringLabel")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement stringLabel;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varStringLabel")
+	@iOSXCUITFindBy(xpath = IOS_VAR_STRING_XPATH)
+	public MobileElement stringLabel;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varString")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement stringValue;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varString")
+	@iOSXCUITFindBy(xpath = IOS_VAR_STRING_XPATH + FOLLOWING_SIBLING_TEXT_XPATH)
+	public MobileElement stringValue;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varNumberLabel")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement numberLabel;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varNumberLabel")
+	@iOSXCUITFindBy(xpath = IOS_VAR_NUMBER_XPATH)
+	public MobileElement numberLabel;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varNumber")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement numberValue;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varNumber")
+	@iOSXCUITFindBy(xpath = IOS_VAR_NUMBER_XPATH + FOLLOWING_SIBLING_TEXT_XPATH)
+	public MobileElement numberValue;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varBoolLabel")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement boolLabel;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varBoolLabel")
+	@iOSXCUITFindBy(xpath = IOS_VAR_BOOL_XPATH)
+	public MobileElement boolLabel;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varBool")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement boolValue;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varBool")
+	@iOSXCUITFindBy(xpath = IOS_VAR_BOOL_XPATH + FOLLOWING_SIBLING_TEXT_XPATH)
+	public MobileElement boolValue;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varFileLabel")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement fileLabel;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varFileLabel")
+	@iOSXCUITFindBy(xpath = IOS_VAR_FILE_XPATH)
+	public MobileElement fileLabel;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varFile")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement fileValue;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varFile")
+	@iOSXCUITFindBy(xpath = IOS_VAR_FILE_XPATH + FOLLOWING_SIBLING_TEXT_XPATH)
+	public MobileElement fileValue;
 
-    @AndroidFindBy(id = "com.leanplum.rondo:id/varFileImageLabel")
-    // @iOSXCUITFindBy(xpath = "")
-    public MobileElement fileImageLabel;
+	@AndroidFindBy(id = "com.leanplum.rondo:id/varFileImageLabel")
+	@iOSXCUITFindBy(xpath = IOS_VAR_FILE_IMAGE_XPATH)
+	public MobileElement fileImageLabel;
 
-    @AndroidFindBy(xpath = ANDROID_IMAGE_XPATH)
-    @iOSXCUITFindBy(xpath = IOS_IMAGE_XPATH)
-    public MobileElement fileImage;
+	@AndroidFindBy(xpath = ANDROID_IMAGE_XPATH)
+	@iOSXCUITFindBy(xpath = IOS_VAR_FILE_IMAGE_XPATH + FOLLOWING_SIBLING_IMAGE_XPATH)
+	public MobileElement fileImage;
 
-    private MobileDriver<MobileElement> driver;
+	private MobileDriver<MobileElement> driver;
 
-    public VariablesPO(MobileDriver<MobileElement> driver) {
-        super(driver);
-        this.driver = driver;
-        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
-    }
+	public VariablesPO(MobileDriver<MobileElement> driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
+	}
 
-    public boolean areValuesCorrect(String expectedString, String expectedNumber, String expectedBoolean,
-            String expectedFile, boolean hasImage) {
+	public boolean areValuesCorrect(String expectedString, String expectedNumber, String expectedBoolean,
+			String expectedFile, boolean hasImage) {
 
-        return getTextFromElement(stringValue).equals(expectedString)
-                && getTextFromElement(numberValue).equals(expectedNumber)
-                && getTextFromElement(boolValue).equals(expectedBoolean)
-                && getTextFromElement(fileValue).equals(expectedFile) && hasImage == hasImage();
-    }
+		return getTextFromElement(stringValue).equals(expectedString)
+				&& getTextFromElement(numberValue).equals(expectedNumber)
+				&& getTextFromElement(boolValue).equals(expectedBoolean)
+				&& getTextFromElement(fileValue).equals(expectedFile) && hasImage() == hasImage;
+	}
 
-    private boolean hasImage() {
-        if (driver instanceof AndroidDriver) {
-            return MobileDriverUtils.doesSelectorMatchAnyElements(driver, ANDROID_IMAGE_XPATH);
-        } else {
-            return MobileDriverUtils.doesSelectorMatchAnyElements(driver, IOS_IMAGE_XPATH);
-        }
-    }
+	private boolean hasImage() {
+		if (driver instanceof AndroidDriver) {
+			return MobileDriverUtils.doesSelectorMatchAnyElements(driver, ANDROID_IMAGE_XPATH);
+		} else {
+			return MobileDriverUtils.doesSelectorMatchAnyElements(driver,
+					IOS_VAR_FILE_IMAGE_XPATH + FOLLOWING_SIBLING_IMAGE_XPATH);
+		}
+	}
 }
