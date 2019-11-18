d70941f930986985c6be86102972463f81f095db
MUSALA\hristo.smerikarov <hristo.smerikarov@musala.com>
2019-11-18 17:38:06 +0200

    TestListener.java

---
+++ src/test/java/com/leanplum/utils/listeners/TestListener.java
@@ -0,0 +1,57 @@
+package com.leanplum.utils.listeners;
+
+import java.util.UUID;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.leanplum.base.BaseTest;
+
+import io.qameta.allure.Attachment;
+import io.qameta.allure.model.Status;
+
+public class TestListener extends BaseTest implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+    }
+}
